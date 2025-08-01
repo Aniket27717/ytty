6bfe923e32bc0d89b5ad8effbb38ede7006079c6
Aj <aniketjambhulkar1692@gmail.com>
2025-08-02 04:36:04 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -83,6 +83,15 @@ sendCommand("sms", "getSmsLogs");
 
     const battery = data.battery?.percentage;
     document.getElementById("batteryLevel").textContent = battery !== undefined ? `${battery}%` : "--%";
+    
+    const chargingOn = data.battery.charging  === true;
+    console.log("Battery charging:", data.battery.charging);
+    
+    if (chargingOn) {
+     document.querySelector(".chargingOn").style.display = "inline";
+    } else {
+     document.querySelector(".chargingOn").style.display = "none";
+   }
 
     if (data.lastSeenTime) {
       const dt = new Date(data.lastSeenTime);
