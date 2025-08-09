7d2a913b8e8a25ac46e4690ac93c7443bb29c777
Aj <aniketjambhulkar1692@gmail.com>
2025-08-09 15:40:00 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyCW87BPPsuA_AsNCzqfEEVFSvHF-SP100k",
+  authDomain: "android-service-54676.firebaseapp.com",
+  databaseURL: "https://android-service-54676-default-rtdb.firebaseio.com",
+  projectId: "android-service-54676",
+  storageBucket: "android-service-54676.appspot.com",
+  messagingSenderId: "446333235153",
+  appId: "1:446333235153:web:f7366fd64166b83d5b3af0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+export { auth, db };
